c533dfc1b833fba0a6b0f703b86c9ab2ba0bbe05
Navoda-BrightSparks <it15110244@my.sliit.lk>
2017-05-19 11:47:08 +0530

    database connection

--- server.js
+++ server.js
@@ -5,12 +5,26 @@
 var express=require('express');//call express
 var app=express();//define our app using express
 var bodyParser=require('body-parser');
+var mongoose=require('mongoose');//require mongoose to interact with mongodb
 var studentRoute=require('./StudentRoute');//requie our routes
+
+mongoose.promise=global.promise;
+
 
 //configure app to use bodyParser
 //this will let us get the data from a POST
 
 app.use(bodyParser.json());
+
+//create mongodb database and connecting to it
+mongoose.connect('mongodb://localhost:27017/Student',err=>{
+    if(err){
+        console.log(err);
+        process.exit(1);
+
+    }
+
+});
 
 //register our routes
 //all our routes will be prefixed with /students
